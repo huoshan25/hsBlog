c7758f2aaf1fcb34ec5ff78d6722b17ec782df39
liliangcan <2633057734@qq.com>
2024-06-23 13:51:21 +0800

    feat: 自定义路由完善

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,4 +1,4 @@
-import type {RouterConfig} from '@nuxt/schema';
+import type { RouterConfig } from '@nuxt/schema';
 
 /*自定义路由*/
 export default <RouterConfig>{
@@ -6,12 +6,20 @@ export default <RouterConfig>{
     let routesDirectory: string | null = null;
     const path = useState('path', () => '')
 
-    if (process.server) {
+    if (import.meta.server) {
       path.value = useDeviceType().type
       routesDirectory = path.value === "pc" ? 'pc' : 'mobile'
     }
 
-    /*清除未使用的目录的功能*/
+    if (import.meta.client) {
+      routesDirectory = path.value === "pc" ? 'pc' : 'mobile'
+    }
+
+    /**
+     * 清除未使用的目录的功能
+     * @param route 路由
+     * @param directory 需要删除的目录
+     */
     function isUnderDirectory(route: any, directory: any) {
       const path = route?.path
       return path === '/' + directory || path.startsWith('/' + directory + '/')
@@ -20,21 +28,23 @@ export default <RouterConfig>{
     /**过滤组件路由 */
     let newRoutes: any = _routes.filter(route => !/\b\/components\b/.test(route.path))
 
-    /* 过滤组件文件夹*/
+    /* 过滤非blog目录和admin目录下的pc和mobile目录*/
     if (routesDirectory) {
       newRoutes = _routes.filter((route: any) => {
         const toRemove = routesDirectory === 'pc' ? 'mobile' : 'pc'
-        return !isUnderDirectory(route, toRemove)
+        /*保留admin目录和blog目录下的路由，并过滤掉blog下的另一个设备目录*/
+        return (isUnderDirectory(route, 'admin') || isUnderDirectory(route, 'blog')) && !isUnderDirectory(route, `blog/${toRemove}`)
       }).map((route) => {
-        if (route) {
+        /*修改blog目录下的路径*/
+        if (isUnderDirectory(route, 'blog')) {
           return {
             ...route,
-            path: route?.path.replace(`/${routesDirectory}`, '/'),
+            path: route?.path.replace(`/blog/${routesDirectory}`, '/blog'),
             name: route.name || 'index'
           }
         }
+        return route
       })
-      console.log(newRoutes, 'newRoutes')
       return newRoutes
     }
   }
