bea8c3777e8370910db6a4c15066c4b96e3ad836
liliangcan <2633057734@qq.com>
2024-07-09 01:10:27 +0800

    feat: nuxt配置（seo）

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,13 +9,13 @@ export default defineNuxtConfig({
       title: '火山博客',
       meta: [
         { charset: 'utf-8' },
-        { name: 'description', content: '火山博客, 技术分享' },
-        { name: 'keywords', content: '火山博客, 后端, vue, react, 技术分享'},
+        { name: 'description', content: '火山博客 - 欢迎来到我的技术学习笔记，这里记录了一名前端开发的成长之旅。从编程基础到最新技术趋势，分享实用的代码片段、个人项目经验和学习资源。无论你是刚入门的新手，还是希望持续学习的同行，都能在这里找到共鸣和帮助。让我们一起探索技术的乐趣，共同进步！' },
+        { name: 'keywords', content: '火山博客,稀土,Vue.js,前端面试题,node,ReactNative,Python,'},
         { name: 'viewport', content: 'width=device-width, initial-scale=1'},
         { name: 'renderer', content: 'webkit' },
         { name: 'author', content: '2633057734@qq.com' }
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/svg/logo.svg' }]
     }
   },
 
