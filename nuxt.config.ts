6742895b45206e7b6e89d4ab0f18371f7b81ca32
liliangcan <2633057734@qq.com>
2024-07-11 01:53:49 +0800

    feat: routeRules配置，启用混合渲染，后台管理页面在客户端渲染，博客首页预渲染。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,21 @@ export default defineNuxtConfig({
     }
   },
 
+  routeRules: {
+    // 主页在构建时预渲染
+    '/blog': { prerender: true },
+    // 产品页面按需生成，后台自动重新验证
+    // '/products/**': { swr: 3600 },
+    // 博客文章按需生成，直到下一次部署前持续有效
+    '/blog/post/**': { isr: true },
+    // 管理仪表板仅在客户端渲染
+    '/admin/**': { ssr: false },
+    // 在API路由上添加cors头
+    '/api/**': { cors: true },
+    // 跳转旧的URL
+    // '/old-page': { redirect: '/new-page' }
+  },
+
   imports: {
     dirs: [
       // 扫描顶级模块
@@ -47,7 +62,7 @@ export default defineNuxtConfig({
   },
 
   devtools: {enabled: true},
-  modules: ['nuxtjs-naive-ui', '@varlet/nuxt', '@vueuse/nuxt', '@unocss/nuxt'],
+  modules: ['nuxtjs-naive-ui', '@varlet/nuxt', '@vueuse/nuxt', '@unocss/nuxt', '@nuxt/image'],
 
   css: ['~/assets/style/default.scss'],
 
