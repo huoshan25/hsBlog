96d0e19dda882c08eada58d7729b99392a126cb8
huoshan <1726941245@qq.com>
2024-10-18 00:12:46 +0800

    TODO: 详情页暂提

---
+++ src/pages/blog/post/[id].vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {getArticleDetails} from "~/api/admin/article";
+
+const route = useRoute()
+definePageMeta({
+  layout: 'blog',
+})
+
+const {data} = await getArticleDetails({id: route.params.id as any})
+console.log(data, 'data')
+</script>
+
+<template>
+  <div class="main">
+    <div class="main-content">
+      <h1>{{ data.title }}</h1>
+    </div>
+
+    <div class="main-info">
+      <n-card
+          title="分类"
+      >
+        卡片内容
+        当前分类的所有文章{{data.category_article_count}}
+      </n-card>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.main {
+  width: 100%;
+  padding: 0 4vw;
+  display: flex;
+  justify-content: center;
+
+  &-content {
+    background-color: white;
+  }
+
+  &-info {
+    background-color: white;
+  }
+}
+</style>
