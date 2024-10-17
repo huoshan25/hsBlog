c2be27f06ecce0a904098f142cb539a37a2aaabc
huoshan <1726941245@qq.com>
2024-10-18 00:16:00 +0800

    TODO: 友链暂提

---
+++ src/pages/blog/friendChains/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'blog',
+});
+
+useHead({
+  title: '关于',
+  titleTemplate: (titleChunk) => `${titleChunk} - 火山博客`
+})
+</script>
+
+<template>
+  <div>友链</div>
+</template>
+
+<style scoped>
+
+</style>
