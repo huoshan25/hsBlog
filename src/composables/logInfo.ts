0afd591cd31372cf54f740db040c7df57744514b
huoshan <1726941245@qq.com>
2024-10-20 22:10:51 +0800

    feat: 控制台打印博客信息

---
+++ src/composables/logInfo.ts
@@ -0,0 +1,17 @@
+/**
+ * log信息
+ */
+export const consoleLogInfo = () => {
+  console.log("%c火山博客: https://github.com/huoshan25/hsBlog",
+    "color: #3491FA; font-size: 14px; font-weight: bold;"
+  );
+  console.log("%c服务端代码（nest）: https://github.com/huoshan25/hsBlog_api",
+    "color: #3491FA; font-size: 14px; font-weight: bold;"
+  );
+  console.log("%c基于 Nuxt 3 的个人博客系统（前后台）",
+    "color: #3491FA; font-size: 14px; font-weight: bold;"
+  );
+  console.log("%c联系邮箱：1726941245@qq.com",
+    "color: #3491FA; font-size: 14px; font-weight: bold;"
+  );
+}
--- src/layouts/blog.vue
+++ src/layouts/blog.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import {type GlobalThemeOverrides} from 'naive-ui'
 import { zhCN, dateZhCN } from 'naive-ui'
+import {consoleLogInfo} from "~/composables/logInfo";
 
 /**获取主题颜色*/
 const { themeColor} = useThemeColor()
 
 /*主题覆盖*/
 const themeOverrides: GlobalThemeOverrides = useThemeOverrides(themeColor);
+
+onMounted(() => {
+  consoleLogInfo()
+})
 </script>
 <template>
   <n-config-provider :theme-overrides="themeOverrides" :locale="zhCN" :date-locale="dateZhCN">
@@ -14,7 +19,7 @@ const themeOverrides: GlobalThemeOverrides = useThemeOverrides(themeColor);
       <n-message-provider>
         <n-modal-provider>
           <blog-navbar/>
-          <div>
+          <div class="mt-[70px]">
             <slot></slot>
           </div>
           <blog-footer/>
