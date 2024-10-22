79fbbde8c9d11522750c4096f79f3d26a2f85210
huoshan <1726941245@qq.com>
2024-10-22 12:00:06 +0800

    feat: 标签列表

--- src/api/blog/tag/index.ts
+++ src/api/blog/tag/index.ts
@@ -1,3 +1,11 @@
+import type {HttpRes} from "~/api/type";
+
+export interface TagsList {
+  id: number;
+  name: string;
+  articleCount: number;
+}
+
 /**
  * 查询全部标签
  */
@@ -10,4 +18,11 @@ export async function getTagsAll() {
  */
 export async function getArticle(params?: any) {
   return await fetchRequest.get<any>('/blog/tag/articles', params);
+}
+
+/**
+ * 模糊搜索标签列表
+ */
+export async function getTagsQuery(params: { keyword: string }) {
+  return await fetchRequest.get<HttpRes<TagsList[]>>('/blog/tag/search', params);
 }
---
+++ src/pages/blog/search/components/tagList.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {getTagsQuery, type TagsList} from "~/api/blog/tag";
+import {HttpStatus} from "~/enums/httpStatus";
+
+const route = useRoute()
+const tagsList = ref<TagsList[]>([])
+
+const getTagsList = async () => {
+  const res = await getTagsQuery({keyword: route.query.keyword as string})
+  if (res.code === HttpStatus.OK) {
+    tagsList.value = res.data
+  }
+}
+watch(() => route.query, getTagsList, {deep: true});
+onMounted(() => {
+  if(tagsList.value.length === 0) {
+    getTagsList()
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-col bg-white w-[100%]">
+    <div class="flex p-[20px]" v-for="tag in tagsList" :key="tag.id">
+      <img class="w-[40px] h-[40px]" src="~/assets/images/tag.png" alt="标签图片">
+      <div class="flex flex-1 flex-col ml-[10px]">
+        <div v-html="tag.name"></div>
+        <div>{{ tag.articleCount }}  文章</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+:deep(em) {
+  color: red;
+  font-style: normal;
+  margin: 0.2em;
+}
+</style>
