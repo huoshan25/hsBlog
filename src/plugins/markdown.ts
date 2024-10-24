955798d8e5f651e7419e855a9220a342cdde8a6b
huoshan <1726941245@qq.com>
2024-10-24 21:01:06 +0800

    feat: 支持语言缩写

--- src/plugins/markdown.ts
+++ src/plugins/markdown.ts
@@ -1,24 +1,37 @@
 import MarkdownIt from 'markdown-it'
 import anchor from 'markdown-it-anchor'
 import hljs from 'highlight.js'
+// import 'highlight.js/styles/hybrid.css'
 import 'highlight.js/styles/vs2015.css'
 
 export default defineNuxtPlugin(() => {
+  const languageAliases: Record<string, string> = {
+    'ts': 'typescript',
+    'js': 'javascript',
+    'py': 'python',
+    'rb': 'ruby',
+    'yml': 'yaml',
+    'sh': 'bash',
+    'rs': 'rust'
+  }
+
   const md = new MarkdownIt({
     html: true,
     linkify: true,
     typographer: true,
     highlight: function (str, lang): string {
-      if (lang && hljs.getLanguage(lang)) {
+      const actualLang = languageAliases[lang] || lang
+
+      if (actualLang && hljs.getLanguage(actualLang)) {
         try {
-          return `<pre class="hljs"><code>${hljs.highlight(str, {
-            language: lang,
+          return `<pre class="hljs p-[15px]"><code>${hljs.highlight(str, {
+            language: actualLang,
             ignoreIllegals: true
           }).value}</code></pre>`
         } catch (__) {
         }
       }
-      return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`
+      return `<pre class="hljs p-[15px]"><code>${md.utils.escapeHtml(str)}</code></pre>`
     }
   })
 
@@ -29,7 +42,7 @@ export default defineNuxtPlugin(() => {
       symbol: '#',
     }),
     level: [1, 2, 3, 4, 5, 6],
-    slugify: () => `heading-${headingIndex++}` // 自动生成 heading-0, heading-1 等
+    slugify: () => `heading-${headingIndex++}`
   })
 
   return {
