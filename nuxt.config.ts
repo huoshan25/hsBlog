155ff345744bda0e33e82b42c13f8cdb7f87a6f9
huoshan <1726941245@qq.com>
2024-10-24 23:47:35 +0800

    feat: 管理爬虫文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -61,6 +61,8 @@ export default defineNuxtConfig({
     /**对客户端暴露的公共密钥*/
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE || 'http://127.0.0.1/',
+      /*网站域名*/
+      siteUrl: process.env.SITE_URL || 'https://myblog.com'
       // gtagId: G-XXXXXX,
     }
   },
