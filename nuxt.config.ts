bfad58aae3d64254fc3045d51be6a79e5811d45b
huoshan <1726941245@qq.com>
2024-10-30 01:08:30 +0800

    feat: 配置调整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -93,19 +93,7 @@ export default defineNuxtConfig({
 
   srcDir: 'src/',
 
-  build: {
-    transpile:
-      process.env.NODE_ENV === 'production'
-        ? ['naive-ui', 'vueuc', '@css-render/vue3-ssr', '@juggle/resize-observer']
-        : ['@juggle/resize-observer']
-  },
-
   vite: {
-    optimizeDeps: {
-      include: process.env.NODE_ENV === 'development'
-        ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
-        : []
-    },
     plugins: [
       AutoImport({
         imports: [
