2b0023b878c348df6850fdf7ea425fca056b91a3
huoshan <1726941245@qq.com>
2024-11-01 15:13:57 +0800

    refactor: 文章许可证重构

--- src/pages/blog/post/components/license.vue
+++ src/pages/blog/post/components/license.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-
 const postLink = computed(() => {
   return location.protocol + '//' + location.host + location.pathname
-
 })
 
 const website = computed(() => {
@@ -11,46 +9,22 @@ const website = computed(() => {
 </script>
 
 <template>
-  <div class="license-wrap">
-    <span>【END】</span>
-    <p>本文链接：{{ postLink }}</p>
+  <div class="relative border border-dashed border-[#ccc] rounded-[5px] p-[13px_16px_8px] mt-[50px] mb-[20px] font-size-[14px] w-full">
+    <span class="absolute left-1/2 -top-[12px] -translate-x-1/2 bg-white font-500 select-none">【END】</span>
+    <p class="mb-[5px]">本文链接：{{ postLink }}</p>
     <p>
       <span>版权声明：本博客所有文章除声明转载外，均采用</span>
       <a
           href="https://creativecommons.org/licenses/by-nc-sa/4.0"
           target="_blank"
-          class="color-#1e80ff mx-[4px]"
+          class="text-[#1e80ff] mx-1"
       >CC-BY-NC-SA 4.0</a>
       <span>许可协议。转载请注明来自</span>
-      <a :href="website" class="color-#1e80ff ml-[4px]">火山博客</a>。
+      <a :href="website" class="text-[#1e80ff] ml-1">火山博客</a>。
     </p>
   </div>
 </template>
 
 <style scoped lang="scss">
-.license-wrap {
-  position: relative;
-  border: 1px dashed #ccc;
-  border-radius: 5px;
-  padding: 13px 16px 8px;
-  margin: 50px 0 20px;
-  font-size: 14px;
-  width: 90%;
-}
 
-.license-wrap > span,
-.end-wrap span {
-  position: absolute;
-  left: 50%;
-  top: -12px;
-  transform: translate(-50%, 0);
-  display: block;
-  background: #fff;
-  font-weight: 500;
-  user-select: none;
-}
-
-.license-wrap > p {
-  margin-bottom: 5px;
-}
 </style>
