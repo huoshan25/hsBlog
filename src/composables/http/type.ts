a383be3db02ba624939bb5c3cfba9323a3dd7fbe
huoshan <1726941245@qq.com>
2025-01-16 14:58:12 +0800

    feat: 开发环境接口代理

--- src/composables/http/type.ts
+++ src/composables/http/type.ts
@@ -1,13 +1,14 @@
-import type {SearchParameters} from "ofetch";
+import type { SearchParameters } from "ofetch";
+import type { HttpRes } from "~/api/type";
 
 export interface RefreshTokenResponse {
-  token: string
-  refresh_token: string
+  token: string;
+  refresh_token: string;
 }
 
 export interface FetchApiFn {
-  get: <T>(url: string, params?: SearchParameters) => Promise<T>
-  post: <T>(url: string, body?: SearchParameters) => Promise<T>
-  put: <T>(url: string, body?: SearchParameters) => Promise<T>
-  delete: <T>(url: string, body?: SearchParameters) => Promise<T>
+  get: <T>(url: string, params?: SearchParameters) => Promise<HttpRes<T>>;
+  post: <T>(url: string, body?: SearchParameters) => Promise<HttpRes<T>>;
+  put: <T>(url: string, body?: SearchParameters) => Promise<HttpRes<T>>;
+  delete: <T>(url: string, body?: SearchParameters) => Promise<HttpRes<T>>;
 }
