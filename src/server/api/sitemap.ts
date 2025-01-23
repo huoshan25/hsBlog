b04c82dc34a616444c23bbaaa210f00c0d0988af
huoshan <1726941245@qq.com>
2025-01-23 20:27:27 +0800

    feat: 文章动态站点地图新增更新时间

--- src/server/api/sitemap.ts
+++ src/server/api/sitemap.ts
@@ -1,20 +1,24 @@
 import type { HttpRes } from "~/api/type";
 
+interface ArticleInfo {
+  id: number;
+  updateTime: string;
+}
+
 export default defineSitemapEventHandler(async () => {
   try {
     const { apiBaseUrl } = useRuntimeConfig().public;
-    const res = await $fetch<HttpRes<number[]>>("/blog/article/article-id-list", {
+    const res = await $fetch<HttpRes<ArticleInfo[]>>("/blog/article/sitemap-info", {
       baseURL: apiBaseUrl,
       method: "GET"
     });
 
-    const data = res.data;
-
-    const routeList = data.map(id => {
-      return { loc: `/blog/post/${id}` };
-    });
-
-    return routeList ?? [];
+    return res.data.map(({ id, updateTime }) => ({
+      loc: `/blog/post/${id}`,
+      lastmod: updateTime,
+      changefreq: "weekly",
+      priority: 0.8
+    }));
   } catch (error) {
     console.error("获取sitemap失败", error);
     return [];
