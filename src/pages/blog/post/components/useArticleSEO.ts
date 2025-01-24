99ef3defdcab6afb93e2d9844a1b943da1f0963a
huoshan <1726941245@qq.com>
2025-01-24 14:56:47 +0800

    refactor: 去除log

--- src/pages/blog/post/components/useArticleSEO.ts
+++ src/pages/blog/post/components/useArticleSEO.ts
@@ -8,8 +8,6 @@ export const useArticleSEO = (article: ArticleDetails) => {
   const config = useRuntimeConfig();
   const route = useRoute();
 
-  console.log(`${config.public.siteUrl}${route.path}`, "`${config.public.siteUrl}${route.path}`");
-
   useHead({
     title: article.title,
     titleTemplate: title => `${title} - 火山博客`,
